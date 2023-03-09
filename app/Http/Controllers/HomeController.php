1caf2e2dfe43853a70cc65b2fe5902ec33f5f088
Jaydee-31 <johndextergamo@gmail.com>
2023-03-09 23:54:33 +0800

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index(){
+        return view('home.index',['team' => Team::all()]);
+    }
+}
